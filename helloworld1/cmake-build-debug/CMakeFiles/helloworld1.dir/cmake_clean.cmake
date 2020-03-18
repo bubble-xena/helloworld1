edcc86c2443c7d3a1ecb12922b386972d6511cd5
xiaosiqi <1843345703@qq.com>
2020-03-18 15:37:36 +0800

    test1

---
+++ helloworld1/cmake-build-debug/CMakeFiles/helloworld1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld1.dir/main.c.o"
+  "helloworld1"
+  "helloworld1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/helloworld1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
